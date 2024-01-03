c1e080644936b3729e4fd7e395302074df19be07
“JuanSeGA” <117958247+JuanSeGArtor@users.noreply.github.com>
2024-01-03 09:33:07 -0500

    modificar Supermarket agregando logica con listas

--- src/Supermarket.java
+++ src/Supermarket.java
@@ -4,14 +4,18 @@ public class Supermarket {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Lista para almacenar productos en el inventario
+        List<Producto> inventario = new ArrayList<>();
+
         // Producto por defecto
-        Producto producto = new Producto(
+        Producto productoD = new Producto(
             "Pera",
             2000,
             true,
             10,
             0
         );
+        inventario.add(productoD);
 
         int op;
         do{
@@ -33,38 +37,53 @@ public class Supermarket {
                     double pPrecio = sc.nextDouble();
                     System.out.println("Indique sí el producto estará disponible a la venta (true/false):");
                     boolean pDisponible = sc.nextBoolean();
-                    System.out.println("Ingresa el las existencias del producto:");
+                    System.out.println("Ingresa las existencias del producto:");
                     int pExistencias = sc.nextInt();
                     System.out.println("Ingresa la cantidad de unidades vendidas sí aplica:");
                     int pUnidadesVendidas = sc.nextInt();
-                    producto = new Producto(pNombre, pPrecio, pDisponible, pExistencias, pUnidadesVendidas);
 
-                    System.out.println("Producto agregado: " + producto);
+                    // Crear nuevo producto y agregarlo a la lista
+                    Producto nuevoProd = new Producto(pNombre, pPrecio, pDisponible, pExistencias, pUnidadesVendidas);
+                    inventario.add(nuevoProd);
+
+                    System.out.println("Producto agregado: " + nuevoProd.getNombre());
                     break;
                 case 2:
                     System.out.println("Precio de venta cada producto");
-                    // Esto iria dentro de un ciclo que iteraria los productos con su precio de un Array Productos
-                    System.out.println("Producto: " + producto.getNombre());
-                    System.out.println("Precio: " + producto.getPrecio());
-                    System.out.println("Existencias: " + producto.getExistencias());
-                    System.out.println("-----------------------------------");
+
+                    for (Producto producto : inventario) {
+                        System.out.println("Producto: " + producto.getNombre());
+                        System.out.println("Precio: " + producto.getPrecio());
+                        System.out.println("Existencias: " + producto.getExistencias());
+                        System.out.println("-----------------------------------");
+                    }
                     break;
                 case 3:
                     System.out.println("Vende los productos disponibles");
-                    producto.vender();
+                    System.out.println("¿Está seguro de VENDER TODOS los productos disponibles? (true/false)");
+                    boolean vender = sc.nextBoolean();
+
+                    if (vender) {
+                        for (Producto producto : inventario) {
+                            if (producto.isDisponible()) {
+                                producto.vender();
+                            }
+                        }
+                    }
                     break;
                 case 4:
                     System.out.println("Elimina productos vendidos");
-                    System.out.println("¿Está seguro de eliminar los productos vendidos? (true/false)");
-                    boolean si = sc.nextBoolean();
+                    System.out.println("¿Está seguro de ELIMINAR los productos vendidos? (true/false)");
+                    boolean eliminar = sc.nextBoolean();
 
-                    if (si) {
-                        // Iría ciclo para eliminar cada producto de Array Productos
-                        if (producto!=null) {
-                            producto.eliminar();
-                            producto = null;
-                        } else {
-                            System.out.println("El producto ya ha sido eliminado.");
+                    if (eliminar) {
+                        Iterator<Producto> iterator = inventario.iterator();
+                        while (iterator.hasNext()) {
+                            Producto producto = iterator.next();
+                            if (producto.getUnidadesVendidas() > 0) {
+                                iterator.remove();
+                                producto.eliminar();
+                            }
                         }
                     }
                     break;
